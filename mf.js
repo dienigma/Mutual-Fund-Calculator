47e895fb3750ed8f30f6b64ce7d3d0c4d6b940db
Nakul <nakulnagargade@gmail.com>
2019-04-07 21:28:30 +0530

    Made some changes to make it work..Now it's Working

--- mf.js
+++ mf.js
@@ -1,19 +1,10 @@
-
-function values(){
-const investmentAmount = document.getElementById("amount").value;
-console.log(investmentAmount);
-const time = parseInt(document.getElementById("time").value);
-console.log(time);
-const annualRate = parseInt(document.getElementById("annualRate").value)
-const rate = annualRate/100;
- console.log(rate)
-}
-
 function calculateReturn() {
-    values();
-    var returnAmount = investmentAmount * (1 + rate/time)^time*time
-    console.log(returnAmount)
-    document.getElementById('returnAmount').innerHTML = `<b> Rs.${returnAmount} </b>`
+    const investmentAmount = document.getElementById("amount").value;
+    const time = parseInt(document.getElementById("time").value);
+    const annualRate = parseInt(document.getElementById("annualRate").value);
+    const rate = annualRate/100;
+    var returnAmount = Math.floor(investmentAmount * (1 + rate/time)**(time*time));
+    document.getElementById('returnAmount').innerHTML = `<h4> Rs.${returnAmount} </h4>`
 }
 
 
